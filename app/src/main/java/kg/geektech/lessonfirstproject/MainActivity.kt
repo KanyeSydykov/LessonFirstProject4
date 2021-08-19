15296325187d6800b3f863884aa4ab2a52be90b6
user <skantoro@inbox.ru>
2021-08-19 19:39:58 +0600

    commit#1

--- app/src/main/java/kg/geektech/lessonfirstproject/MainActivity.kt
+++ app/src/main/java/kg/geektech/lessonfirstproject/MainActivity.kt
@@ -1,17 +1,17 @@
 package kg.geektech.lessonfirstproject
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import kg.geektech.lessonfirstproject.databinding.ActivityMainBinding
 
+const val edit = "edit1"
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    @SuppressLint("ShowToast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -24,15 +24,25 @@ class MainActivity : AppCompatActivity() {
             val edit1: String = binding.intentEditTextActivityMain.text.toString()
 
             if (binding.intentEditTextActivityMain.text.toString() == "") {
-                Toast.makeText(this, " Please enter some message! ",
-                    Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    this, getString(R.string.warning1),
+                    Toast.LENGTH_SHORT
+                ).show()
             } else {
                 val intent = Intent(this, SecondActivity::class.java)
-                intent.putExtra("edit1", edit1)
-                startActivity(intent)
+                intent.putExtra(edit, edit1)
+                startActivityForResult(intent, oboba)
+                setResult(RESULT_OK, intent)
+                finish()
             }
         }
 
+
+    }
+
+    companion object {
+
+        val oboba = 0
 
     }
 
--- app/src/main/java/kg/geektech/lessonfirstproject/SecondActivity.kt
+++ app/src/main/java/kg/geektech/lessonfirstproject/SecondActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.widget.Toast
 import kg.geektech.lessonfirstproject.databinding.ActivitySecondBinding
 
+const val edit3 = "edit2"
+
 class SecondActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySecondBinding
@@ -18,17 +20,20 @@ class SecondActivity : AppCompatActivity() {
         val edit1: String? = intent.getStringExtra("edit1")
         binding.intentEditTextActivitySecond.setText(edit1)
 
-
         binding.intentButtonActivityMain.setOnClickListener {
             val edit2: String = binding.intentEditTextActivitySecond.text.toString()
 
             if (binding.intentEditTextActivitySecond.text.toString() == "") {
-                Toast.makeText(this, " Please enter some message! ",
-                    Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    this, getString(R.string.warning1),
+                    Toast.LENGTH_SHORT
+                ).show()
             } else {
                 val intent = Intent(this, MainActivity::class.java)
-                intent.putExtra("edit2", edit2)
-                startActivity(intent)
+                intent.putExtra(edit3, edit2)
+                startActivityForResult(intent, MainActivity.oboba)
+                setResult(RESULT_OK, intent)
+                finish()
             }
         }
 
